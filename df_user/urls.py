1c4d5ea3cee26b11ba9c6d7966876fd5a4388996
Davenny <2429367410@qq.com>
2017-12-03 16:24:45 +0800

    First commit

--- df_user/urls.py
+++ df_user/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url(r'^register/$',views.register),
-    url(r'^register_handle/$',views.register_handle),
-    url(r'^login/$',views.register),
+    url(r'^login/$',views.register_handle),
 ]
