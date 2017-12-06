b3451289176f4ff4395306905a4fe5d7c381f79b
Davenny <2429367410@qq.com>
2017-12-07 00:11:06 +0800

    Finish home page

--- df_goods/views.py
+++ df_goods/views.py
@@ -9,6 +9,6 @@ def Index(request):
     typelist = TypeInfo.objects.all()
     type0 = typelist[0].goodsinfo_set.order_by('-id')[0:4]
     type01 = typelist[0].goodsinfo_set.order_by('-gclick')[0:4]
-    context = {'title':'首页','guest_cart':'1',
+    context = {'title':'首页','guest_cart':1,
                'type0':type0,'type01':type01}
     return render(request,'df_good/index.html',context)
